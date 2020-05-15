12eae873b679d9a81986ca220d428645e7880382
Hector Flores <frhectorin@gmail.com>
2020-05-14 19:39:31 -0500

    Login

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+import Login from './Components/Login.js'
+
+function App() {
+  return (
+    <div className="body">
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
